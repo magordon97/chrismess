3ca518948c5daa8add243fa30ef6476ef0adfa88
Matthew Gordon <magordon@anderson.edu>
2018-06-26 10:27:59 -0400

    Form added. Accepts text input and updates heading with click of button or enter key

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 console.log('It works!')
 
-const updateHeadingButton = document.querySelector("button")
+const updateHeadingButton = document.querySelector('#Button1')
 
 const updateHeadingFunction = function (){
-    const heading = document.querySelector('#Heading2')
-    heading.textContent = "No, wait. Chris Evans is the best Chris!"
+    const headingUpdate = document.querySelector('#Heading2')
+    headingUpdate.textContent = "No, wait. Chris Evans is the best Chris!"
 }
 
-updateHeadingButton.onclick = updateHeadingFunction
+updateHeadingButton.addEventListener ('click', updateHeadingFunction)
+
+const submitChrisButton = document.querySelector('#Button2')
+
+const submitChrisFunction = function (){
+    const headingSubmit = document.querySelector('#Heading1')
+    headingSubmit.textContent = document.querySelector('#textInput1').value
+    //alert(headingSubmit.textContent)
+    event.preventDefault()
+}
+
+submitChrisButton.addEventListener ('click', submitChrisFunction)
