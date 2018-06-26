96de378c08d71e72bc1025f6538d9c2982810bfa
Matthew Gordon <magordon@anderson.edu>
2018-06-25 22:14:34 -0400

    Button added, updates heading 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('It works!')
+
+const updateHeadingButton = document.querySelector("button")
+
+const updateHeadingFunction = function (){
+    const heading = document.querySelector('h1')
+    heading.textContent = "CHRIS!"
+}
+
+updateHeadingButton.onclick = updateHeadingFunction
