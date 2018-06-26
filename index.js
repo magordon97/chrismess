f298e2947b357c6f00ebb58bc7aa713d7b003ef0
Matthew Gordon <magordon@anderson.edu>
2018-06-25 22:39:46 -0400

    Second heading added, button updates second heading

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log('It works!')
 const updateHeadingButton = document.querySelector("button")
 
 const updateHeadingFunction = function (){
-    const heading = document.querySelector('h1')
-    heading.textContent = "CHRIS!"
+    const heading = document.querySelector('#Heading2')
+    heading.textContent = "No, wait. Chris Evans is the best Chris!"
 }
 
 updateHeadingButton.onclick = updateHeadingFunction
