acd751ac5afe37f3acaf8d64caa2cb07e2fcd1e2
Matthew Gordon <magordon@anderson.edu>
2018-06-27 16:28:45 -0400

    Add class and constructor. Change const to functions. Need to fix style issue in span

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
-console.log('It works!')
+class App{
+    constructor(){
+        const form1 = document.querySelector("form#Form1")
+        form1.addEventListener ('submit', (ev)=>{
+            ev.preventDefault()
+            this.submitChrisMovie(ev)
+        })
+    }
 
-const form1 = document.querySelector("form#Form1")
+    newSpan(name, value){
+        const span = document.createElement('span')
+        span.classList.add(name)
+        span.textContent = value
+        return span
+    }   
 
-const newSpan = function (name, value){
-    const span = document.createElement('span')
-    span.classList.add(name)
-    span.textContent = value
-    return span
-}
+    renderItem(flickObj){
+        const newListElement = document.createElement('li')
+        newListElement.classList.add('flickObj')
 
-const submitChrisMovieFunction = function (ev){
-    ev.preventDefault()
-    const f = ev.target
-
-    const flickName = newSpan('movieNameStyle',f.flickName.value)
-    const flickYear = newSpan('movieYearStyle',` (${f.flickYear.value})`)
-
-    const list1 = document.querySelector('#flicksList')
-
-    //const movie = document.createElement('movieNameStyle')
-    //movie.textContent=flickName
+        const properties = Object.keys(flickObj)
     
-    //const year = document.createElement('movieYearStyle')
-    //year.textContent = flickYear
+        properties.forEach((propertyName)=>{
+            const buildSpan = this.newSpan(propertyName, flickObj[propertyName])
+            newListElement.appendChild(buildSpan) //ERROR HERE
+        })
 
-    const listElement = document.createElement('li')
+        return newListElement
+    }
 
-    listElement.append(flickName)
-    listElement.append(flickYear)
-    listElement.appendChild(document.createElement('br'))
+    submitChrisMovie(ev){        
+        const f = ev.target
 
-    list1.appendChild(listElement)
-    /*
-    const newItem = document.createElement('li')
-    newItem.textContent = flickName + ' (' + flickYear + ')'
+        const flickObj = {
+            name: f.flickName.value,
+            year: ` (${f.flickYear.value})`,
+        }
 
-    const list = document.querySelector('#flicksList')
-    list.appendChild(newItem)
-    */
+        //const flickName = newSpan('movieNameStyle',f.flickName.value)
+        //const flickYear = newSpan('movieYearStyle',` (${f.flickYear.value})`)
 
-  f.reset()
+        const listElement = this.renderItem(flickObj)
+        const list1 = document.querySelector('#flicksList')
+        list1.appendChild(listElement)
+
+        //const movie = document.createElement('movieNameStyle')
+        //movie.textContent=flickName
+    
+        //const year = document.createElement('movieYearStyle')
+        //year.textContent = flickYear
+
+        //const listElement = document.createElement('li')
+
+        //listElement.append(flickName)
+        //listElement.append(flickYear)
+        //listElement.appendChild(document.createElement('br'))
+
+        //list1.appendChild(listElement)
+        /*
+        const newItem = document.createElement('li')
+        newItem.textContent = flickName + ' (' + flickYear + ')'
+
+        const list = document.querySelector('#flicksList')
+        list.appendChild(newItem)
+        */
+    f.reset()
+    }
 }
 
-form1.addEventListener ('submit', submitChrisMovieFunction)
+const app = new App()
+
+console.log('It works!')
